e90a3fddfcd5dec4ecb52de1824bcb229db490d3
Cristian Bernardo <cristian.silva@ceca.ufal.br>
2021-07-06 23:02:48 -0300

    Criado lista de pedidos e correção de bugs

--- viveiro-vivo-mobile/App.tsx
+++ viveiro-vivo-mobile/App.tsx
@@ -1,27 +1,14 @@
-import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import AppLoading from 'expo-app-loading';
-import Header from './src/Header';
-import Home from './src/Home';
+import { StyleSheet, View } from 'react-native';
+import Routes from './src/Routes';
 
 export default function App() {
-
-  let [fontsLoaded] = useFonts({
-    OpenSans_400Regular,
-    OpenSans_700Bold
-  });
-
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
 
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
-      <Header />
-      <Home />
+      <Routes />
     </View>
   );
 }
--- viveiro-vivo-mobile/src/Home/index.tsx
+++ viveiro-vivo-mobile/src/Home/index.tsx
@@ -1,16 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import Header from '../Header';
 
 export default function Home() {
 
-    const handleOnPress = () => {
+    const navigation = useNavigation();
 
+    const handleOnPress = () => {
+        navigation.navigate('Orders');
     }
 
     return (
         <>
+            <Header />
             <View style={styles.container}>
                 {<Image source={require('../assets/logo-raposa-celular.png')} style={styles.logo} />}
                 <Text style={styles.title}>Acompanhe os pedidos e {'\n'} entregue no prazo!</Text>
